99034a913c47f700d9f80fb343b60483d0adbb3f
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-14 23:41:56 -0300

    estruturas condicionais1 Python, if else elif.

---
+++ estruturas-condicionais.py
@@ -0,0 +1,13 @@
+profit = 10
+
+if profit >= 9:
+    print ("Profit aceitável, mas falta 1kk para bater a meta.")
+
+elif profit >= 8:
+    print ("Ok, faltam 2kk para bater a meta.")
+
+elif profit >= 7:
+    print ("Aconteceu alguma coisa? Faltam 3kk para bater a meta, precisamos melhorar!")
+
+else:
+    print ("Talvez seja melhor procurar outro método para melhorar o profit e atingir a meta.")
