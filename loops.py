94c931484f62ce4d3e3a7c8b693baf1cdc4310e4
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-16 21:26:47 -0300

    for e while - loops - permição para repetir um bloco de código várias vezes

---
+++ loops.py
@@ -0,0 +1,19 @@
+# for variável in sequência:
+    # Bloco de Código a repetir
+    # instruções
+
+frutas = ["maça", "banana", "laranja"]
+
+for frutas in frutas:
+    print(frutas)
+
+# while condição:
+    # Bloco de código a repetir
+    # instruções
+
+contador = 0 
+
+while contador <= 5:
+
+    print(contador)
+    contador += 1
