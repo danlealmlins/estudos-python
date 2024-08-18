e574361479ccd3425b5559153693d512c0c54b0f
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-18 16:49:12 -0300

    Informações sobre as estruturas de controle adicionadas

--- estruturas-condicionais.py
+++ estruturas-condicionais.py
@@ -1,3 +1,11 @@
+# As estruturas de controle nos permitem controlar o fluxo de execução de nossos programas. 
+# Em Python, as estruturas de controle mais comuns são as estruturas condicionais e os loops. 
+# Essas estruturas nos permitem tomar decisões e repetir blocos de código segundo certas condições.
+
+# Estruturas condicionais
+# As estruturas condicionais nos permitem executar diferentes blocos de código segundo se cumpra ou não uma determinada condição.
+# Em Python, as estruturas condicionais mais utilizadas são if, if-else e if-elif-else.
+
 # Profit diário esperado
 profit_esperado = 10 # representando 10kk
 
