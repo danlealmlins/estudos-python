eac50acd6ebe464eabc5aa398b902bc61025aecf
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-18 16:54:44 -0300

    Questão sobre estruturas condicionais

---
+++ exercicio1.py
@@ -0,0 +1,18 @@
+num = 7 
+
+if num % 2 == 0:
+    print("O número é par.")
+
+# Essa expressão "% 2 == 0" indica que estamos verificando se o numero é par.
+# O operador % é o operador de módulo, que retorna o resto da divisão de dois números.
+# 2 é o número pelo qual estamos dividindo.
+# == é o operador de igualdade, que compara dois valores para ver se são iguais.
+# 0 é o valor com o qual estamos comparando o resto.
+
+else:
+    print("O número é impar.")
+
+# Portanto, num % 2 == 0 retorna True se num for divisível por 2 (ou seja, se o resto da divisão de num por 2 for igual a 0). 
+# Isso significa que num é um número par. Caso contrário, retorna False, indicando que num é ímpar.
+    
+# RESPOSTA = O número é impar, pois num = 7.
