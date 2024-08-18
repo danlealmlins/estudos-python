84b4f59d4c19bf88ff6b2da29a1ed97bc900451a
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-18 17:09:55 -0300

    Questão sobre a função multiplicar.

---
+++ exercicio2.py
@@ -0,0 +1,21 @@
+# Definição da função multiplicar que aceita dois argumentos "a" e "b".
+# "a" e "b" são os parâmetros que a função recebe
+# return a * b, indica que a função deve retornar o produto de "a" e "b" quando for chamada.
+
+def multiplicar (a,b):
+    return a*b
+
+resultado = multiplicar(5, 3) + multiplicar(2, 4)
+print("O resultado é:", resultado)
+
+# Chamada da função e atribuição do resultado
+# multiplicar(5, 3): substituindo "a" por 5 e "b" por 3, a função retorna 5 * 3 = 15
+# multiplicar(2, 4): substituindo "a" por 2 e "b" por 4, a função retorna 2 * 4 = 8.
+
+# soma dos resultados = 15 + 8 = 23.
+
+# Portanto a resposta da questão é: 
+# O código define uma função multiplicar, 
+# essa função calcula dois produtos diferentes (5*3 e 2*4)
+# soma os produtos e armazena o resultado final na variável "resultado" 
+# Resultado = 23
