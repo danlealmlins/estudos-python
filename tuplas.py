8047ad1ade9590b8bc756ff283a2a6c6b7c418e4
Daniel Leal Martiniano Lins <daniel.leal.dev@gmail.com>
2024-08-18 16:51:44 -0300

    estudo sobre Tuplas. Existem dúvidas a estudar.

---
+++ tuplas.py
@@ -0,0 +1,20 @@
+# Uma tupla é uma estrutura de dados imutável e ordenada que permite armazenar uma coleção de elementos. 
+# Os elementos de uma tupla são encerrados entre parênteses (), separados por vírgulas.
+
+# Criação e acesso
+# Para criar uma tupla, encerre os elementos entre parênteses:
+
+ponto = (3, 4)
+
+print(ponto[0])  # Imprime 3
+
+print(ponto[1])  # Imprime 4
+
+# procurar entender Métodos de tuplas
+
+# count(elemento): devolve o número de vezes que um elemento aparece na tupla. 
+# index(elemento): devolve o índice da primeira aparição de um elemento na tupla. Opcionalmente, pode-se especificar o início e fim da busca. 
+# len(tupla): embora não seja um método de tupla propriamente dito, esta função incorporada devolve o comprimento da tupla.
+
+# /\ Dúvida nesse assunto.
+
